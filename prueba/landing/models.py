7048337aa32d29945328e64576fad7b41f9a7aa8
EstebanGVX <esterockr1997@gmail.com>
2023-04-15 21:54:56 -0500

    primer commit

---
+++ prueba/landing/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=50, null=False)
+    password = models.CharField(max_length=50)
+
+class Operations(models.Model):
+    dateInit = models.DateField(auto_now=False,auto_now_add=True)
+    time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    boton1 = models.IntegerField(default=0)
+    boton2 = models.IntegerField(default=0)
+    timeInit = models.DateTimeField(auto_now=False, auto_now_add=False)
+    timeFinal = models.DateTimeField(auto_now=False, auto_now_add=False)
+    idUser = models.ForeignKey(Users, on_delete=models.CASCADE)
